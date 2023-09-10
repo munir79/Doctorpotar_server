c11bb399bb388369c4975c133743dae0979e30b6
munir79 <jakirhossainmunir79@gmail.com>
2023-09-10 11:45:30 +0600

    refetch

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express=require('express');
-const cors =require('cors');
+const express = require('express');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
-const app=express();
-const port=process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
 
 
 
@@ -31,7 +31,43 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const appoinmentOption=client.db('doctorportal').collection('appoinmentoption')
+    const appoinmentOption = client.db('doctorportal').collection('appoinmentoption');
+    const bookingCollection = client.db('doctorportal').collection('bookings');
+    app.get('/appoinment', async (req, res) => {
+      const date = req.query.date;
+      console.log(date);
+      const bookingQuery = { appointmentDate: date }
+      const alreadyBooked = await bookingCollection.find(bookingQuery).toArray();
+      console.log(alreadyBooked);
+      const query = {};
+      const cursor = appoinmentOption.find(query);
+      const option = await cursor.toArray();
+      option.forEach(option => {
+        const optionBooked = alreadyBooked.filter(book => book.treatmentName == option.name);
+        const bookslot = optionBooked.map(book => book.slot);
+        const remainningSlots = option?.slots?.filter(slot => !bookslot.includes(slot));
+       option.slots=remainningSlots;
+        console.log(date,option.name ,remainningSlots?.length);
+   
+
+      })
+      res.send(option);
+    })
+
+    /*
+ app.get('/read all from database ')
+app.get('/readone from database/:id')
+app.post('/post a data to database')
+app.patch('/update/:id');
+app.delete('/delete/:id);
+    */
+
+    app.post('/bookings', async (req, res) => {
+      const booking = req.body
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    })
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
@@ -39,7 +75,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-   // await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -65,11 +101,11 @@ run().catch(console.dir);
 
 
 
- app.get('/',(req,res)=>{
-    res.send('hello from node mongo server ');
- })
+app.get('/', (req, res) => {
+  res.send('hello from node mongo server ');
+})
 
 
- app.listen(port ,()=>{
-    console.log(`listening to port ${port} `);
- })
+app.listen(port, () => {
+  console.log(`listening to port ${port} `);
+})
